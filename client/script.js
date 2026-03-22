7316f329a55ec36d5512d0e3d5652d66f1b7d9a0
Muraripothuru <murari.pothuru098@gmail.com>
2026-03-22 21:49:20 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,1 +1,16 @@
+async function checkFact() {
+    const text = document.getElementById("news").value;
 
+    const response = await fetch("http://localhost:5000/api/fact-check", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ text })
+    });
+
+    const data = await response.json();
+
+    document.getElementById("result").innerText =
+        `Result: ${data.result}`;
+}
