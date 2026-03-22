504332a001debf14b3922f2f6e3a38f9855ab3a8
Muraripothuru <murari.pothuru098@gmail.com>
2026-03-22 21:47:39 +0530

    Add fact checking and translation functionality

--- server/controllers/factcontroller.js
+++ server/controllers/factcontroller.js
@@ -1,1 +1,20 @@
+const { translateText } = require('../services/translateService');
+const { factCheck } = require('../services/factCheckService');
 
+exports.checkFact = async (req, res) => {
+    try {
+        const { text } = req.body;
+
+        const translated = await translateText(text);
+        const result = await factCheck(translated);
+
+        res.json({
+            original: text,
+            translated,
+            result
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: "Error processing request" });
+    }
+};
